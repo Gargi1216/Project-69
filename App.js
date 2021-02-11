854cc8d9c9e1c9030d2f3d1ebf2ed7dc6d15bad1
Gargi Gupta <67412979+Gargi1216@users.noreply.github.com>
2021-02-11 18:56:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import ScanScreen from './screens/ScanScreen';
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <View>
+        <Text>QR Code Scanner</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
